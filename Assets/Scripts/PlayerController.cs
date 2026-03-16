2ba3aa6e79a7b5f4f4dbfd28c36a801e9cfebd58
nabil aici <cybernaboo@yahoo.fr>
2026-03-16 22:16:24 +0100

    update 16/03/2026 22:16:24

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,26 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-
     }
 
     void FixedUpdate()
     {
         float x = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * x * speed * Time.fixedDeltaTime);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Car"))
+        {
+            GameObject.Find("Road").GetComponent<Scrolling>().enabled = false;
+            GameObject.Find("Grass").GetComponent<Scrolling>().enabled = false;
+            // list of script Rolling in the child components of the player
+            Rolling[] rollings = GetComponentsInChildren<Rolling>();
+            foreach (Rolling rolling in rollings)
+            {
+                rolling.enabled = false;
+            }
+
+        }
     }
 }
